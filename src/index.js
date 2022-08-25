2466387f8fa6de028eb398713f646181b676da92
Declan Davis <dexthefish@gmail.com>
2022-08-25 14:59:59 -0400

    improve Square component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 function Square(props) {
-  return <h1>square</h1>;
+  const [state, setState] = useState(props.value);
+  return (
+    <button className="square" onClick={() => setState("x")}>
+      {state}
+    </button>
+  );
 }
 
 function Board(props) {
-  return <h1>board</h1>;
+  return (
+    <>
+      <div>game status</div>
+      <div className="row">
+        <Square value={0} />
+        <Square value={1} />
+        <Square value={2} />
+      </div>
+      <div className="row">
+        <Square value={3} />
+        <Square value={4} />
+        <Square value={5} />
+      </div>
+      <div className="row">
+        <Square value={6} />
+        <Square value={7} />
+        <Square value={8} />
+      </div>
+    </>
+  );
 }
 
 function Game() {
   return (
     <>
       <Board />
-      <Square />
     </>
   );
 }
