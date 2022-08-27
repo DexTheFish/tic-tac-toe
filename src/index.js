5a630fc85a95e3fb40eeecdde89c0a60c12465e4
Declan Davis <dexthefish@gmail.com>
2022-08-26 20:42:34 -0400

    add click handler in Board component

--- src/index.js
+++ src/index.js
@@ -4,34 +4,41 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 function Square(props) {
-  const [state, setState] = useState(props.value);
+  // props:
+  // value
+  // onClick
   return (
-    <button className="square" onClick={() => setState("x")}>
-      {state}
+    <button className="square" onClick={props.onClick}>
+      {props.value}
     </button>
   );
 }
 
 function Board(props) {
-  const [values, setValues] = useState(Array(9).fill(null));
-  console.log(values);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  console.log(squares);
+  const handleClick = (i) => {
+    const squaresCopy = squares.slice();
+    squaresCopy[i] = "X";
+    setSquares(squaresCopy);
+  };
   return (
     <>
       <div>game status</div>
       <div className="row">
-        <Square value={values[0]} />
-        <Square value={values[1]} />
-        <Square value={values[2]} />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="row">
-        <Square value={values[3]} />
-        <Square value={values[4]} />
-        <Square value={values[5]} />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="row">
-        <Square value={values[6]} />
-        <Square value={values[7]} />
-        <Square value={values[8]} />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </>
   );
