7cc4649dde89f4d0460d2cfa600ab82dd55b25a5
Declan Davis <dexthefish@gmail.com>
2022-08-31 17:44:31 -0400

    add function to check for winner

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ function Board(props) {
     setSquares(squaresCopy);
     setX(!x);
   };
+
   return (
     <>
       <div>Next to move: {x ? "X" : "O"}</div>
@@ -53,3 +54,26 @@ function Game() {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+function winner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (const line of lines) {
+    if (
+      squares[line[0]] &&
+      squares[line[0]] === squares[line[1]] &&
+      squares[line[1]] === squares[line[2]]
+    ) {
+      return squares[line[0]];
+    }
+  }
+  return null;
+}
